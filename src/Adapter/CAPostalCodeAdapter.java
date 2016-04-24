55784216b5932d4c81e32b9842c16a876ab1a392
smilebing <iblandshot@outlook.com>
2016-04-24 20:57:35 +0800

    Adapter

---
+++ src/Adapter/CAPostalCodeAdapter.java
@@ -0,0 +1,14 @@
+package Adapter;
+
+public class CAPostalCodeAdapter extends CanadianPostalCode implements ZipCodeValidator  {
+
+	  private CanadianPostalCode objCPostCode;
+
+	  public CAPostalCodeAdapter(CanadianPostalCode pCode) {
+	     objCPostCode = pCode;
+	  }
+	  public boolean isValidZipCode(String zip, String state) {
+	      return isValidCanadianPcode(zip, state);
+	  }
+	}// end of class
+	
---
+++ src/Adapter/ZipCodeValidator.java
@@ -0,0 +1,7 @@
+package Adapter;
+
+public interface ZipCodeValidator {
+
+	   public abstract boolean isValidZipCode(String zip, String state);
+
+	}// end of class
